c0023041311a36d4928b3a31654b6a3a6a66f1bc
Natalia Garrido <natgramirez@outlook.com>
2017-06-02 21:12:50 -0400

    agregando estilos para work con flex y cambios de navbar con scroll

--- js/app.js
+++ js/app.js
@@ -1,3 +1,24 @@
 /*
  * Archivo principal de funcionalidad de JS
  */
+
+/***************** Función para hacer cambio de navbar con el evento scroll en window ***********************/
+
+function cambioDeNavbar() {
+    var distanciaY = window.pageYOffset || document.documentElement.scrollTop;
+    // La navbar cambia de color al de la sección correspondiente al llegar a ella
+    var navbar = document.getElementById('navbar');
+    if (distanciaY > 600) {
+        navbar.classList.add("menu-color");
+    } else {
+        navbar.classList.remove("menu-color");
+    }
+
+    // Al salir de la sección vuelve a su color original -> blanco
+    if (distanciaY > 1400) {
+        navbar.classList.remove("menu-color");
+    }
+}
+
+// Evento para el navegador al hacer scroll que detona la función que provoca los cambios de color en la navbar segun la distancia
+window.addEventListener('scroll', cambioDeNavbar);
