5d1c6e997781153945104368e80190268b547063
Natalia Garrido <natgramirez@outlook.com>
2017-06-02 21:40:36 -0400

    agregando funcionalidad a la galeria con modales

--- js/app.js
+++ js/app.js
@@ -22,3 +22,54 @@ function cambioDeNavbar() {
 
 // Evento para el navegador al hacer scroll que detona la función que provoca los cambios de color en la navbar segun la distancia
 window.addEventListener('scroll', cambioDeNavbar);
+
+
+/********************************* Reutilizando Modales de applove ************************************************/
+
+(function modalesPorFoto() {
+    // Array.from para que reconozca la clase como un arreglo
+    var fotos = Array.from(document.getElementsByClassName('img-galery'));
+    // Id perteneciente al div donde ubicaremos el modal
+    var modalFoto = document.getElementById('work-galery-modal');
+    var modalGaleria, close, img;
+    fotos.forEach(function(foto) {
+        console.log(foto);
+        foto.addEventListener('click', function() {
+            // Borro todo lo que esta dentro del modal
+            modalFoto.innerHTML = '';
+            // En modalGaleria crearemos el div que contendra al modal
+            modalGaleria = document.createElement('div');
+            // Le creamos la clase modal-galeria donde incluiremos la foto, y sus estilos en main.css
+            modalGaleria.classList.add('modal-galeria');
+            modalGaleria.innerHTML = foto.innerHTML;
+
+            //Hacemos hide el modal para antes del click
+            modalFoto.appendChild(modalGaleria);
+            modalFoto.classList.remove('hide');
+
+            // La imagen x para cerrar la foto ira en otro div
+            close = document.createElement('div');
+            // En la clase close se tratan los estilos para la x
+            close.classList.add('close');
+
+            // Se agrega la imagen a img con src
+            img = document.createElement('img');
+            img.setAttribute('src', 'https://www.iconfinder.com/data/icons/geomicons/32/672366-x-128.png');
+            img.setAttribute('class', 'opacity-img');
+
+            // Se apenda la img de x a close y esto al modal foto
+            close.appendChild(img);
+            modalFoto.appendChild(close);
+
+            // Cuando se haga click en close se escondera el modal nuevamente
+            close.addEventListener('click', function() {
+                modalFoto.classList.add('hide');
+            });
+
+            var close = document.querySelector('.close');
+            close.addEventListener('click', function() {
+                modalFoto.classList.add('hide');
+            });
+        });
+    });
+})();
